4d6fe5a1f384846b061d2a55f3e9e08456ef8bc0
PU6ER <djakobstv@gmail.com>
2024-01-28 21:56:13 +0300

    feat: update css

--- src/components/ui/carousel/Carousel.tsx
+++ src/components/ui/carousel/Carousel.tsx
@@ -8,7 +8,7 @@ import styles from './Carousel.module.scss'
 
 export default function Carousel() {
 	return (
-		<div className='flex justify-center mt-10'>
+		<div className='flex justify-center mt-10 w-full'>
 			<div className='w-[900px] inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]'>
 				<ul className='flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll'>
 					<li>
