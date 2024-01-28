b692b1fd244a3e02ded1a7d3fdbb7a81ac5354f7
PU6ER <djakobstv@gmail.com>
2024-01-28 21:49:53 +0300

    feat: add new section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Carousel from '@/components/ui/carousel/Carousel'
 import Hero from '@/components/ui/hero/Hero'
+import Skills from '@/components/ui/skills/Skills'
 
 export default function Home() {
 	return (
-		<div>
+		<div className=''>
 			<Hero />
 			<Carousel />
+			<Skills />
 		</div>
 	)
 }
