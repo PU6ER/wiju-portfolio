2729bb7c00757661f01dcbc4ebbe04b48347dc07
PU6ER <djakobstv@gmail.com>
2024-01-28 14:51:05 +0300

    feat: add carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Carousel from '@/components/ui/carousel/Carousel'
 import Hero from '@/components/ui/hero/Hero'
 
 export default function Home() {
 	return (
 		<div>
 			<Hero />
+			<Carousel />
 		</div>
 	)
 }
