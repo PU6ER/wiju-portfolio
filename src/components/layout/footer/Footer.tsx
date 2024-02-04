9823fd34f0996f1793a3aee4b1a6395d4b903629
PU6ER <djakobstv@gmail.com>
2024-02-04 15:10:14 +0300

    feat: add basic responsiveness

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,13 +1,12 @@
 import { FileSearch, Github, Mail, MapPin, Phone, Send } from 'lucide-react'
 import Image from 'next/image'
-import Link from 'next/link'
 import styles from './Footer.module.scss'
 
 export default function Footer() {
 	return (
-		<footer className='h-[150px] w-full flex flex-col  items-center overflow-x-hidden'>
+		<footer className='sm:h-[150px] h-[150px] w-screen flex flex-col  items-center overflow-x-hidden px-3'>
 			<hr className='h-px w-full  border-top border-blue-600' />
-			<nav className={styles.navbar}>
+			<section className={styles.navbar}>
 				<div className='flex flex-row gap-2'>
 					<Image alt='logo' src='logo.svg' width={30} height={20} />
 					<div className=''>
@@ -15,38 +14,36 @@ export default function Footer() {
 						<span className=' text-green-500'>.dev</span>
 					</div>
 				</div>
-				<div className={styles.links}>
-					<Link href='/blog'>Blog</Link>
-					<Link href='/blog'>Portfolio</Link>
-					<Link href='/blog'>Portfolio</Link>
-					<Link href='/blog'>Portfolio</Link>
+				<div
+					className='hidden  lg:flex border p-5 rounded-xl border-blue-600'
+					id='contacts'
+				>
+					<div className='flex gap-3'>
+						<Mail color='#1E54F9' />
+						<span>wijudev@gmail.com</span>
+					</div>
+					<div className='flex border-l ml-3 pl-3 gap-3'>
+						<Phone color='#22C55E' />
+						<span>+79998887444</span>
+					</div>
+					<div className='flex border-l ml-3 pl-3 gap-3'>
+						<MapPin color='#F71919' />
+						<span>Moscow, Russia</span>
+					</div>
 				</div>
-				<div className='flex'>
+				<div className='flex gap-3 items-center justify-center flex-row md:flex-row lg:flex-row xl:flex-row'>
 					<div className='bg-primary p-1 rounded-lg cursor-pointer'>
 						<Github />
 					</div>
-					<div className='bg-primary p-1 rounded-lg cursor-pointer ml-3'>
+					<div className='bg-primary p-1 rounded-lg cursor-pointer'>
 						<Send />
 					</div>
-					<div className='bg-primary p-1 rounded-lg cursor-pointer ml-3'>
+					<div className='bg-primary p-1 rounded-lg cursor-pointer '>
 						<FileSearch />
 					</div>
 				</div>
-			</nav>
-			<div className='flex border p-5 rounded-xl border-blue-600'>
-				<div className='flex'>
-					<Mail color='#1E54F9' className='mr-3' />
-					<span>wijudev@gmail.com</span>
-				</div>
-				<div className='flex border-l ml-3 pl-3'>
-					<Phone color='#22C55E' className='mr-3' />
-					<span>+79998887444</span>
-				</div>
-				<div className='flex border-l ml-3 pl-3'>
-					<MapPin color='#F71919' className='mr-3' />
-					<span>Moscow, Russia</span>
-				</div>
-			</div>
+			</section>
+
 			<span className=' h-5 text-xs mt-2 text-gray-400'>
 				Copyrigth © 2024 Vladislav Hasanov. All rights reserved.
 			</span>
