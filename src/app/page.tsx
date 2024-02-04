82bd96d512cd09d31a283b0d1b0e09852c4ea6a7
PU6ER <djakobstv@gmail.com>
2024-02-04 15:09:26 +0300

    feat: add new sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Carousel from '@/components/ui/carousel/Carousel'
 import Hero from '@/components/ui/hero/Hero'
 import Skills from '@/components/ui/skills/Skills'
+import Works from '@/components/ui/works/Works'
 
 export default function Home() {
 	return (
@@ -8,6 +9,7 @@ export default function Home() {
 			<Hero />
 			<Carousel />
 			<Skills />
+			{/* <Works /> */}
 		</div>
 	)
 }
